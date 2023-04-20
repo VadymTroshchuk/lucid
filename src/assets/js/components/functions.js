5bb09d67806515e282eec8ed44610c2ebf0248db
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-04-21 02:03:48 +0300

    fix: fix header position, links behaviour and other small fixes

--- src/assets/js/components/functions.js
+++ src/assets/js/components/functions.js
@@ -155,3 +155,23 @@ function smoothScrollToTop() { // adjusting scroll speed when moving to top of t
     }
   }, 15);
 }
+
+
+
+
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+
+  window.addEventListener('scroll', () => {
+    // get the current scroll position
+    const scrollPos = window.scrollY;
+  console.log(scrollPos)
+    // setting the background color of the header based on the scroll position
+    if (scrollPos > 60) {
+      header.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // changing backround color
+    } else {
+      header.style.backgroundColor = ''; // remove the background color 
+    }
+  });
+
+})
